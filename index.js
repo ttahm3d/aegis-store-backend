b54788538e7d3f8b023b1cc3aeb2434ac7d94e60
ttahm3d <ttahm3d@gmail.com>
2022-05-29 23:02:48 +0530

    feat: connected everything together

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import express from "express";
-const app = express();
+import "dotenv/config";
+import cors from "cors";
+import { connect } from "./configs/mongoDbConfig.js";
 
-app.listen("4000", () => {
-  console.log("App is running");
+// Defining Routes
+import categoryRoutes from "./routes/category.routes.js";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+connect();
+
+app.get("/", (req, res) => {
+  res.send("message");
+});
+
+app.use("/api/v1", categoryRoutes);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`[index.js] Application is running on ${port} `);
 });
