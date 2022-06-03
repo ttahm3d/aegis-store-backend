8c356a9158cc13e2dbf940e7ec87b321652dec9c
ttahm3d <ttahm3d@gmail.com>
2022-06-03 22:35:43 +0530

    feat: created middleware

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,22 @@
+import jsonwebtoken from "jsonwebtoken";
+
+const isAuthenticated = async (req, res, next) => {
+  const token =
+    req.cookies.token ||
+    (req.header("Authorization")?.split(" ")[0] === "Bearer" &&
+      req.header("Authorization").split(" ")[1]);
+  if (!token)
+    return res.status(404).send({ message: "Authorization Token is required" });
+  try {
+    const decodedToken = await jsonwebtoken.decode(token);
+    if (!decodedToken) {
+      return res.status(404).send({ message: "Invalid token" });
+    }
+    req.user_Id = decodedToken.id;
+    next();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+export { isAuthenticated };
